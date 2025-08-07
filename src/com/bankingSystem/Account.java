40946533d3b541b7016f7965a7a65a81ae025ddc
pooja <poojaeb123@gmail.com>
2025-08-07 14:28:27 +0530

    Remove bankingSystem package from remote repo

--- src/com/bankingSystem/Account.java
+++
@@ -1,7 +0,0 @@
-package com.bankingSystem;
-
-public abstract class Account {
-
-	public abstract void withdraw(int amount, Customer customer);
-	public abstract void calculateInterest(Customer customer);
-}
